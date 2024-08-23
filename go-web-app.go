164c277808d4f261e395cae4c0d8b2463ed3f96c
mailvelous <mailvelous0@gmail.com>
2024-08-23 14:25:23 +0700

    nothing

--- go-web-app.go
+++ go-web-app.go
@@ -5,4 +5,24 @@ import (
 	"net/http"
 )
 
-func 
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	var message = "Welcome"
+	w.Write([]byte(message))
+}
+
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+	var message = "Hello world"
+	w.Write([]byte(message))
+}
+
+func main() {
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/index", handlerIndex)
+    http.HandleFunc("/hello", handlerHello)
+
+    fmt.Println("Starting server on :8080...")
+    err := http.ListenAndServe(":8080", nil)
+    if err!= nil {
+        panic(err)
+    }
+}
